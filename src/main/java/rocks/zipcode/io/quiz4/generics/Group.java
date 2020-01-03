0e5802b48796739a6c6231e9ebd5c7ecb9024760
Julia Evans <julevans93@users.noreply.github.com>
2020-01-03 11:59:31 -0500

    working on generics

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,34 +1,46 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author leon on 18/12/2018.
  */
-public class Group<_> {
+public class Group<_> implements Iterable{
+
+    private List<_> group;
+
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+
+        this.group = new ArrayList<>();
     }
 
     public Integer count() {
-        return null;
+
+        return group.size();
     }
 
     public void insert(_ value) {
+        group.add(value);
     }
 
     public Boolean has(_ value) {
-        return null;
+
+        return group.contains(value);
     }
 
     public _ fetch(int indexOfValue) {
-        return null;
+
+        return group.get(indexOfValue);
     }
 
     public void delete(_ value) {
+        group.remove(value);
     }
 
     public void clear() {
+        group.clear();
     }
 
     public Iterator<_> iterator() {
